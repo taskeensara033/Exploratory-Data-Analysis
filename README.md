589cd74aac633c5eca0e745e0bf62c0a86efd017
Sara Taskeen <taskeensara033@gmail.com>
2025-08-01 21:54:01 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+#Exploratory Data Analysis
+
+<p>This project performs a detailed analysis of Retails saled data to identify <strong>which types of customers spend the most.</strong>The Analysis focuses on several factors including <strong>gender,age group, state, occupation, product category, and marital status</strong>
+<br>
+The process involves:<br>
+<li><strong>Data Cleaning:</strong>Removing unwanted columns, handling missing values, and standardizing column names.</li>
+<li><strong>Data Transformation:</strong>Mapping coded values (eg.,0 -> Single, 1 -> Married) and formatting gender labels for clarity.</li>
+<li><strong>Exploratory Data Analysis:</strong>Using grouping and aggregation to calculate total spending for each demographic factor.</li>
+<li><strong>Visualisation:</strong>Creating clear charts that show spending patterns across different customer segments.</li>
--- src/Sales project.py
+++ src/Sales project.py
@@ -2,115 +2,137 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df=pd.read_csv('sales_data(1).csv', encoding='ISO-8859-1')
-print(df) 
-
-# Displaying all the rows
-pd.set_option('display.max_rows',None)
-print(df)
-
-
-# Displaying all the columns
-pd.set_option('display.max_columns',None)
-print(df)
+# print(df)
+# pd.set_option('display.max_rows',None)
+# print(df)
+# print(df['Amount'],df['Gender'])
 
 
-#Finding null values in Amount column
-print(df[df['Amount'].isnull()])
+# pd.set_option('display.max_columns',None)
 
+#**********************Finding null values in Amount column************
+# print(df[df['Amount'].isnull()])
 
-#droping status and unnammed1 column
+#**********************droping status and unnammed1 column************
 df.drop(['Status','unnamed1'],axis=1,inplace=True)    ## by default axis is 0 it considers rows
-print(df)
+# print(df)
 
-#Deleting nan values
-pd.set_option('display.max_rows',None)
-print(df.dropna(inplace=True))
+#**********************Deleting nan values************************
+# pd.set_option('display.max_rows',None)
+# print(df.dropna(inplace=True))
 
 
-#Renaming columns
+#**********************Renaming columns************************
 df.rename(columns={'Marital_Status':'Marital'},inplace=True)
-print(df)
+# print(df)
 
 
-# change values of marital 0-->single  1-->married
+# #**********************change values of marital 0-->single  1-->married************************
 df['Marital']=df['Marital'].replace({0:'Single',1:'Married'})
-print(df)
+# # # print(df)
 
 df['Gender']=df['Gender'].replace({'F':'Female','M':'Male'})
-print(df)
+# print(df)
+
+# #**********************Visualise counts pf male and female************************
+# sns.countplot(x='Gender',data=df,palette='bright')
+# plt.show()
+
+# v=sns.countplot(x='Gender',data=df,palette='dark')
+# for i in v.containers:
+#     v.bar_label(i)
+# plt.show() 
+
+# #**********************Group By************************
+# v1=sns.countplot(x='Age Group',data=df,hue='Gender',palette='dark')
+# for i in v1.containers:
+#     v1.bar_label(i)
+# plt.show()
+
+# a=df.groupby('Age Group')['Amount'].sum()
+# Agroup=pd.DataFrame(a)
+# # plt.figure(figsize=(22,5))
+# sns.barplot(x=Agroup.index,y=Agroup.Amount,palette='bright')
+# plt.title('Amount Spent by Age Group')
+# plt.ylabel('Amount Spent')
+# plt.show()
 
 
-#Visualise counts pf male and female
-v=sns.countplot(x='Gender',data=df,palette='dark')
-for i in v.containers:
-    v.bar_label(i)
-plt.show() 
-plt.savefig('Count')
 
-
-#Group By Age Group
-a=df.groupby('Age Group')['Amount'].sum()
-Agroup=pd.DataFrame(a)
+# #**********************Group By************************
 # plt.figure(figsize=(22,5))
-sns.barplot(x=Agroup.index,y=Agroup.Amount,palette='bright')
-plt.title('Amount Spent by Age Group')
-plt.ylabel('Amount Spent')
-plt.show()
+# v2=sns.countplot(x='State',data=df,palette='bright')
+# for i in v2.containers:
+#     v2.bar_label(i)
+#     v2.set_label('State')
+# plt.show()
 
 
-#Group By State
-a=df.groupby('State')['Amount'].sum()
-state=pd.DataFrame(a)
-plt.figure(figsize=(12,6))
-sns.barplot(x=state.index,y=state.Amount,palette='bright')
-plt.title('Amount Spent by State')
-plt.ylabel('Amount Spent')
-plt.xticks(rotation=60,ha='center')
-plt.tight_layout()
-plt.show()
-plt.savefig('State')
+# a=df.groupby('State')['Amount'].sum()
+# state=pd.DataFrame(a)
+# plt.figure(figsize=(12,6))
+# sns.barplot(x=state.index,y=state.Amount,palette='deep')
+# plt.title('Amount Spent by State')
+# plt.ylabel('Amount Spent')
+# plt.xticks(rotation=60,ha='center')
+# plt.tight_layout()
+# plt.show()
 
 
-#Group By Occupation
-a=df.groupby('Occupation')['Amount'].sum()
-state=pd.DataFrame(a)
-sns.barplot(x=state.index,y=state.Amount,palette='coolwarm')
-plt.title('Amount Spent by Occupation')
-plt.ylabel('Amount Spent')
-plt.xticks(rotation=45,ha='right')
-plt.tight_layout()
-plt.show()
-plt.savefig('Occupation')
+# #**********************Group By************************
+# plt.figure(figsize=(22,5))
+# v3=sns.countplot(x='Occupation',data=df,palette='bright')
+# for i in v3.containers:
+#     v3.bar_label(i)
+# plt.show()
+
+# a=df.groupby('Occupation')['Amount'].sum()
+# state=pd.DataFrame(a)
+# sns.barplot(x=state.index,y=state.Amount,palette='coolwarm')
+# plt.title('Amount Spent by Occupation')
+# plt.ylabel('Amount Spent')
+# plt.xticks(rotation=45,ha='right')
+# plt.tight_layout()
+# plt.show()
 
 
-#Group By Product category
-a=df.groupby('Product_Category')['Amount'].sum()
-state=pd.DataFrame(a)
-sns.barplot(x=state.index,y=state.Amount,palette='dark')
-plt.title('Amount Spent based on Product_Category')
-plt.ylabel('Amount Spent')
-plt.xticks(rotation=45,ha='right')
-plt.tight_layout()
-plt.show()
-plt.savefig('Product_Category')
+
+# #**********************Group By************************
+
+# a=df.groupby('State')['Amount'].sum()
+# state=pd.DataFrame(a)
+# plt.figure(figsize=(22,5))
+# sns.barplot(x=state.index,y=state.Amount)
+# plt.show()
 
 
-#Group By Marital Status
-a=df.groupby('Marital')['Amount'].sum()
-state=pd.DataFrame(a)
-sns.barplot(x=state.index,y=state.Amount,palette='dark')
-plt.title('Amount Spent based on Marital_Status')
-plt.ylabel('Amount Spent')
-plt.show()
-plt.savefig('Marital')
 
 
-#Group By Gender
-a=df.groupby('Gender')['Amount'].sum()
-gender=pd.DataFrame(a)
-plt.figure(figsize=(5,5))
-sns.barplot(x=gender.index,y=gender.Amount,palette='dark')
-plt.title('Total Spending by Gender')
-plt.ylabel('Amount Spent')
-plt.show()
-plt.savefig('Gender')
+# #**********************Group By************************
+
+# a=df.groupby('Product_Category')['Amount'].sum()
+# state=pd.DataFrame(a)
+
+# sns.barplot(x=state.index,y=state.Amount,palette='dark')
+# plt.title('Amount Spent based on Product_Category')
+# plt.ylabel('Amount Spent')
+# plt.xticks(rotation=45,ha='right')
+# plt.tight_layout()
+# plt.show()
+
+# #**********************Group By************************
+
+# a=df.groupby('Marital')['Amount'].sum()
+# state=pd.DataFrame(a)
+# sns.barplot(x=state.index,y=state.Amount,palette='dark')
+# plt.title('Amount Spent based on Marital_Status')
+# plt.ylabel('Amount Spent')
+# plt.show()
+
+# a=df.groupby('Gender')['Amount'].sum()
+# gender=pd.DataFrame(a)
+# plt.figure(figsize=(5,5))
+# sns.barplot(x=gender.index,y=gender.Amount,palette='dark')
+# plt.title('Total Spending by Gender')
+# plt.ylabel('Amount Spent')
+# plt.show()
